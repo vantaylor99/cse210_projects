358adae3853f627cea9cc70f8b33ade2bed573b2
vantaylor99 <jacob.van.taylor@gmail.com>
2025-03-05 12:30:22 -0700

    Third assignement done.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+       
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = 0;
+            string higherLower = "";
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string guessFromUser = Console.ReadLine();
+                guess = int.Parse(guessFromUser);
+
+                if (guess > magicNumber)
+                {
+                    higherLower = "Lower";
+                }
+                else if (guess < magicNumber)
+                {
+                    higherLower = "Higher";
+                }
+                else
+                {
+                    Console.WriteLine("Correct! ");
+                }
+                Console.WriteLine(higherLower);
+            }
+            
+        
+        Console.Write("Would you like to play again? ");
+        string keepPlayingInput = Console.ReadLine();
+        keepPlaying = keepPlayingInput;
+        }
+        
+        Console.WriteLine("Thanks for playing!");
     }
 }
