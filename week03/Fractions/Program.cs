d81105e0105e63eaeaf690c1ca194e279eba3fb4
vantaylor99 <jacob.van.taylor@gmail.com>
2025-03-13 16:10:57 -0600

    finished assignment although I never used the getters and setters inside my code.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction_1 = new Fraction();
+        Console.WriteLine(fraction_1.GetFractionString());
+        Console.WriteLine(fraction_1.GetDecimalValue());
+
+        Fraction fraction_2 = new Fraction(5);
+        Console.WriteLine(fraction_2.GetFractionString());
+        Console.WriteLine(fraction_2.GetDecimalValue());
+
+        Fraction fraction_3 = new Fraction(3, 4);
+        Console.WriteLine(fraction_3.GetFractionString());
+        Console.WriteLine(fraction_3.GetDecimalValue());
+
+        Fraction fraction_4 = new Fraction(1, 3);
+        Console.WriteLine(fraction_4.GetFractionString());
+        Console.WriteLine(fraction_4.GetDecimalValue());
+
     }
 }
---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,62 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // public int GetTop()
+    // {
+    //     return _top;
+    // }
+
+    // public int SetTop(int top)
+    // {
+    //     top = _top;
+    //     return top;
+    // }
+
+    // public int GetBottom()
+    // {
+    //     return _bottom;
+    // }
+
+    // public int SetBottom(int bottom)
+    // {
+    //     bottom = _bottom;
+    //     return bottom;
+    // }
+
+    public string GetFractionString()
+    {
+       
+        string fractionString = $"{_top}/{_bottom}";
+        return fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+
+
+
+} 
