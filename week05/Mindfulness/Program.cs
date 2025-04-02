c79d67423754082b0877f2f32057a0cc3027c8d8
vantaylor99 <jacob.van.taylor@gmail.com>
2025-04-01 20:29:38 -0600

    Tiny fix.

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -19,7 +19,7 @@ class Program
             Console.Clear();
             Console.WriteLine("Menu Options:");
             Console.WriteLine("  1. Start breathing activity");
-            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  2. Start reflection activity");
             Console.WriteLine("  3. Start listing activity");
             Console.WriteLine("  4. Quit");
             Console.Write("Select a choice from the menu: ");
